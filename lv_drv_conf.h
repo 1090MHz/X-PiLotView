75308218d31b99fce6a9dc66b9b5b506ac80a1d7
1090MHz <107646764+1090MHz@users.noreply.github.com>
2025-05-31 14:47:52 -0600

    Define USE_FBDEV

--- lv_drv_conf.h
+++ lv_drv_conf.h
@@ -219,7 +219,7 @@
  *  Linux frame buffer device (/dev/fbx)
  *-----------------------------------------*/
 #ifndef USE_FBDEV
-#  define USE_FBDEV           0
+#  define USE_FBDEV           1
 #endif
 
 #if USE_FBDEV
